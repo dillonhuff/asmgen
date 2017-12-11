77f9e04efb3eae6ab9d3c9c50dc9f25db7067cab
Dillon Huff <dillonhuff@gmail.com>
2017-12-11 15:41:16 -0800

    Now adding a real test of conv_3_1

--- test/gencode/test_conv_3_1.cpp
+++ test/gencode/test_conv_3_1.cpp
@@ -1,5 +1,25 @@
 #include "conv_3_1.h"
 
+#include <bitset>
+#include <iostream>
+
+using namespace std;
 int main() {
+  layout s;
+
+  s.self_in_0 = 1;
+  s.self_clk = 1;
+  s.self_clk_last = 0;
+  
+
+  s.lb_p4_clamped_stencil_update_stream$mem_2$waddr$reg0 = 0; // Offset = 0
+  s.lb_p4_clamped_stencil_update_stream$mem_2$raddr$reg0 = 0; // Offset = 1
+  s.lb_p4_clamped_stencil_update_stream$mem_1$waddr$reg0 = 0; // Offset = 2
+  s.lb_p4_clamped_stencil_update_stream$mem_1$raddr$reg0 = 0; // Offset = 3
+
+  conv_3_1(&s);
+
+  cout << "s.self_out = " << (int) s.self_out << endl;
+
   return 0;
 }
