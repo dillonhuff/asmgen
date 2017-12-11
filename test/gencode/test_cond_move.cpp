ede186d2a9d39f86ee4949742213a0bd17d410e0
Dillon Huff <dillonhuff@gmail.com>
2017-12-10 21:08:32 -0800

    Fixed cond move issue

--- test/gencode/test_cond_move.cpp
+++ test/gencode/test_cond_move.cpp
@@ -9,25 +9,25 @@ using namespace std;
 
 int main() {
 
-  cout << "size of mx = " << sizeof(mx) << endl;
+  cout << "size of mx = " << sizeof(layout) << endl;
   layout s;
 
-  s.a = 12;
-  s.b = 3;
+  s.in0 = 12;
+  s.in1 = 3;
   s.sel = 0;
-  s.res = 0;
+  s.out = 0;
 
   cond_move(&s);
 
-  cout << "s.res = " << s.res << endl;
-  assert(s.res == 12);
+  cout << "s.out = " << s.out << endl;
+  assert(s.out == 12);
 
   s.sel = 1;
-  s.res = 0;
+  s.out = 0;
 
   cond_move(&s);
 
-  assert(s.res == 3);
+  assert(s.out == 3);
   
   return 0;
 }
