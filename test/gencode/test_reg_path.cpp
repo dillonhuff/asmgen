1dc58eeb87c562dae9659b41baeeab19eb00d816
Dillon Huff <dillonhuff@gmail.com>
2017-12-10 20:30:31 -0800

    Added real unit test for register functionality

--- test/gencode/test_reg_path.cpp
+++ test/gencode/test_reg_path.cpp
@@ -1,5 +1,20 @@
 #include "reg_path.h"
 
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
 int main() {
+  layout lt;
+  lt.self_in_0 = 1;
+  lt.self_in_1 = 3;
+
+  reg_path(&lt);
+
+  assert(lt.self_out_0 == 4);
+
+  cout << "output = " << lt.self_out_0 << endl;
+
   return 0;
 }
