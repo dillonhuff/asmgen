07250247f05958b6ffcacd2451fb0a605d4be154
Dillon Huff <dillonhuff@gmail.com>
2017-12-12 14:10:10 -0800

    Memory is too long

--- test/gencode/test_memory.cpp
+++ test/gencode/test_memory.cpp
@@ -1,9 +1,36 @@
 #include "memory.h"
 
+#include <cassert>
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+void print_memory(const layout& s) {
+  for (uint i = 0; i < 10; i++) {
+    cout << "m0[ " << i << " ] = " << bitset<16>(s.m0[i]) << endl;
+  }
+}
+
 int main() {
   layout s;
   s.self_clk = 1;
   s.self_clk_last = 0;
+  s.self_write_en = 1;
+  s.self_write_addr = 1;
+  s.self_write_data = 12;
+
+  cout << "Before write" << endl;
+  print_memory(s);
+
+  memory(&s);
+
+  cout << "After write" << endl;
+  print_memory(s);
+  
+  assert(s.m0[s.self_write_addr] == s.self_write_data);
+
+  
 
   return 0;
 }
