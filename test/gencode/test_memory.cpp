94f8a0f8fe77884f2cb1c5f7970ba2942e75e34f
Dillon Huff <dillonhuff@gmail.com>
2017-12-12 15:36:02 -0800

    Need proper enable implementation

--- test/gencode/test_memory.cpp
+++ test/gencode/test_memory.cpp
@@ -20,6 +20,10 @@ int main() {
   s.self_write_addr = 1;
   s.self_write_data = 12;
 
+  s.m0[0] = 0;
+  s.m0[1] = 0;
+  
+
   cout << "Before write" << endl;
   print_memory(s);
 
@@ -31,6 +35,29 @@ int main() {
   cout << "Before assert" << endl;
 
   assert(s.m0[s.self_write_addr] == s.self_write_data);
+  assert(s.m0[0] == 0);
+
+  s.self_write_data = 5;
+
+  memory(&s);
+
+  print_memory(s);
+
+  assert(s.m0[s.self_write_addr] == s.self_write_data);
+
+  cout << "Write without enable" << endl;
+  s.self_write_en = 0;
+  auto old_write_data = s.self_write_data;
+
+  s.self_write_data = 234;
+
+  memory(&s);
+
+  cout << "After no enable write" << endl;
+  print_memory(s);
+
+  assert(s.m0[s.self_write_addr] == old_write_data);
+  
 
   return 0;
 }
