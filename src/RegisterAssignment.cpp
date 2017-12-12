fe929b4c71b6df901909cbd1a09be5fe2fb8274c
Dillon Huff <dillonhuff@gmail.com>
2017-12-12 15:25:34 -0800

    Conv 3 1 is now segfaulting, but at least register allocation works!

--- src/RegisterAssignment.cpp
+++ src/RegisterAssignment.cpp
@@ -248,9 +248,9 @@ void appendAssignRegisters(DataGraph& dg,
     }
 
     alreadyAllocated.insert(node);
-    // afk::concat(x86_32Bit, nowDeadRegisters(alreadyAllocated,
-    //                                         dg,
-    //                                         asg.registerAssignment));
+    afk::concat(x86_32Bit, nowDeadRegisters(alreadyAllocated,
+                                            dg,
+                                            asg.registerAssignment));
 
     x86_32Bit = sort_unique(x86_32Bit);
     x86_32Bit = afk::intersection(x86_32Bit, allx86_32Bit);
